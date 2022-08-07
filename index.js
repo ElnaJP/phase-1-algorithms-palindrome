760093ab8f3c7b6214ce1c9a22f6160087f26bed
Elna JP <eriosdd@gmail.com>
2022-08-07 11:48:18 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let letterArray = word.split("");
+  l = letterArray.length
+ for (let i = 0; i < (l-1)/2; i++){
+   if (letterArray[i] !== letterArray[l-1-i]){
+     return false
+   }
+ } return true
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  
+// 4 letters
+console.log("Expecting: true");
+console.log("=>", isPalindrome("deed"));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+// 3 letters
+console.log("Expecting: true");
+console.log("=>", isPalindrome("nun"));
+
+console.log("");
+
+// 6 letters
+console.log("Expecting: true");
+console.log("=>", isPalindrome("redder"));
+
+console.log("");
+
+// 7 letters
+console.log("Expecting: true");
+console.log("=>", isPalindrome("rotator"));
+
+console.log("");
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("racecar"));
+
+console.log("");
+
+// 5 letters 
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
+
+console.log("");
+
+// 9 letters 
+console.log("Expecting: true");
+console.log("=>", isPalindrome("redivider"));
+
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("hello"));
+
+console.log("");
 }
 
 module.exports = isPalindrome;
+
+
